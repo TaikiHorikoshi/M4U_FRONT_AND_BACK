25e1422672945c0432316c3bbf1cdb051cc1bc22
Taiki Horikoshi <6431503118@lamduan.mfu.ac.th>
2022-11-18 12:43:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+
+const cors = require('cors');
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(cors());
+
+const hbs = require('hbs');
+
+
+
+
+app.use(fileUpload());
+
+app.use(express.urlencoded({ extended: true}));
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+app.use('/static', express.static('static'));
+
+
+app.get('/', (req, res) => {
+    res.render('main_page');
+
+});
+
+app.get('/admission', (req, res) => {
+    res.render('admission');
+
+});
+
+app.get('/renew', (req, res) => {
+    res.render('renew');
+
+});
+
+
+
+app.listen(3001, () => {
+    console.log('start');
+    console.log('http://localhost:3001/');
+})
